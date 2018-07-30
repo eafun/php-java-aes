74bd2126b2eb34e43a1d40579bd1c9d217e7ade0
XingYuZhang <363686276@qq.com>
2018-07-30 09:53:57 +0800

    Update AesDepondOnOpenssl.php

--- AesDepondOnOpenssl.php
+++ AesDepondOnOpenssl.php
@@ -11,8 +11,7 @@ class Aes
      */
     public function encrypt($encrypt, $key)
     {
-        $ivlen = openssl_cipher_iv_length($this->cipher);
-        $iv = openssl_random_pseudo_bytes($ivlen);
+        $iv = $this->getIv();
         $key = substr(openssl_digest(openssl_digest($key, 'sha1', true), 'sha1', true), 0, 16);
         $encrypted = openssl_encrypt($encrypt, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
         return strtoupper(bin2hex($encrypted));
@@ -26,10 +25,16 @@ class Aes
     public function decrypt($decrypt, $key)
     {
         $decoded = $this->hex2bin($decrypt);
-        $ivlen = openssl_cipher_iv_length($this->cipher);
-        $iv = openssl_random_pseudo_bytes($ivlen);
+        $iv = $this->getIv();
         $key = substr(openssl_digest(openssl_digest($key, 'sha1', true), 'sha1', true), 0, 16);
         return openssl_decrypt($decoded, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
+    }
+
+
+    public function getIv()
+    {
+        $ivlen = openssl_cipher_iv_length($this->cipher);
+        return openssl_random_pseudo_bytes($ivlen);
     }
 
     /**
